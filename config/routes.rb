bdbd1b79f594adc7c9342151b3469405b9722a41
TCHALLA Nina Essowaza <nina.tchalla@gmail.com>
2019-08-06 13:23:29 +0200

    Features implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  resources :tasks
+
+  # get 'tasks', to: 'tasks#index'
+
+  # # Create task
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
+
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # # Update task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # # delete
+  # delete 'tasks/:id', to: 'tasks#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
